23173104ddc95f111fcd9313a2ee89a2e2e4a097
Poojitha Machrla <poojitha.machrla@gmail.com>
2025-09-02 18:45:00 +0530

    Added Day 35

---
+++ Day35/main.py
@@ -0,0 +1,20 @@
+import requests
+
+parameters = {
+    "lon": 78.4744,
+    "lat": 17.3753,
+    "appid" : "6ae9ba2394ca9adf046d44496999ddb7",
+    "cnt": 4
+}
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast",params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    print("Bring an umbrella")
